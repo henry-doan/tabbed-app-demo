1c07c8102ca43543ba21f52bf372d17077dfd3e2
Henry Doan <henrydoan96@gmail.com>
2021-06-23 23:35:50 -0600

    got tabbed bar to work in new xcode

---
+++ Tabbed App Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabbed App Demo
+//
+//  Created by Henry Doan on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
